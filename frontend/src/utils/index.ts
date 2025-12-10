e96a45421256f37914e90625d4e176510882597e
RobyDoby <64599889+RobyDoby@users.noreply.github.com>
2025-12-10 20:37:27 +0300

    create project

---
+++ frontend/src/utils/index.ts
@@ -0,0 +1,13 @@
+export async function subscribeUser(): Promise<PushSubscription | null> {
+  const registration = await navigator.serviceWorker.ready;
+
+  const permission = await Notification.requestPermission();
+  if (permission !== "granted") return null;
+
+  const subscription = await registration.pushManager.subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: import.meta.env.VITE_PUBLIC_VAPID_KEY
+  });
+
+  return subscription;
+}
