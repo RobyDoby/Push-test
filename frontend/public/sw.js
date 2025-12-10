2a14e5538066b5edef04d19f61e3350c4dd0548d
RobyDoby <64599889+RobyDoby@users.noreply.github.com>
2025-12-11 00:26:59 +0300

    Update sw.js

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,21 +1,6 @@
-// Важно: этот код должен быть в начале SW файла
-console.log('Service Worker загружен');
-
-self.addEventListener('install', event => {
-  console.log('Service Worker: install event');
-  // Пропускаем ожидание
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', event => {
-  console.log('Service Worker: activate event');
-  // Забираем контроль над всеми клиентами
-  event.waitUntil(self.clients.claim());
-});
-
 self.addEventListener('push', (event) => {
   const data = event.data ? JSON.parse(event.data.text()) : null
-
+  
   if (!data) return
 
   event.waitUntil(
