515fbc170a49ddad2726389601af4a1acc4703e5
RobyDoby <64599889+RobyDoby@users.noreply.github.com>
2025-12-11 13:21:57 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
 )
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/sw.js"
+  navigator.serviceWorker.register("/Push-test/sw.js"
   //   , {
   //   updateViaCache: 'none',
   //   scope: '/'
